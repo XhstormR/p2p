fa9c1004c4728346540d0c242bf83dd240842dcf
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-04-09 15:13:30 +0300

    add alert notification

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -5,13 +5,18 @@ import { MatSnackBar } from "@angular/material/snack-bar";
     providedIn: "root",
 })
 export class NotificationService {
+    private alert = new Audio("assets/sounds/alert.m4a");
+
     constructor(
         private ngZone: NgZone,
         private snackBar: MatSnackBar,
-    ) {}
+    ) {
+        this.alert.load();
+    }
 
     public open(message: string, duration: number = 10_000) {
         this.ngZone.run(() => this.snackBar.open(message, "OK", { duration: duration }));
+        this.alert.play();
     }
 
     public error(message: string, duration: number = 10_000) {
