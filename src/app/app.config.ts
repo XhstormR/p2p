33674db3845eadb73fef3a567f18a20e6877b973
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-06-08 15:18:55 +0300

    Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from "@angular/core";
 import { PreloadAllModules, provideRouter, withPreloading } from "@angular/router";
 import { routes } from "./app.routes";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideHttpClient } from "@angular/common/http";
 import { ErrorHandlerService } from "./service/error-handler.service";
 
@@ -9,7 +8,6 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes, withPreloading(PreloadAllModules)),
-        provideAnimationsAsync(),
         provideHttpClient(),
         { provide: ErrorHandler, useClass: ErrorHandlerService },
     ],
